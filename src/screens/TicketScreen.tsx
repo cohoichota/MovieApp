9a433bd2e1ad39ded73d5595119d169a4cc7a368
cohoichota <cohoichota@gmail.com>
2023-10-07 18:36:42 +0700

    Ticket Screen

--- src/screens/TicketScreen.tsx
+++ src/screens/TicketScreen.tsx
@@ -1,14 +1,251 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {
+  Image,
+  ImageBackground,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import AppHeader from '../components/AppHeader';
+import {
+  BORDERRADIUS,
+  COLORS,
+  FONTFAMILY,
+  FONTSIZE,
+  SPACING,
+} from '../theme/theme';
+import LinearGradient from 'react-native-linear-gradient';
+import CustomIcon from '../components/CustomIcon';
 
-const TicketScreen = () => {
+interface TempDate {
+  date: number;
+  day: string;
+}
+
+interface TicketData {
+  seatArray: number[];
+  time: string;
+  date: TempDate;
+  ticketImage: string;
+}
+
+const TicketScreen = ({navigation, route}: any) => {
+  const [ticketData, setTicketData] = useState<TicketData>(route.params);
+
+  useEffect(() => {
+    const getLocalStorageData = async () => {
+      try {
+        const ticket = await EncryptedStorage.getItem('ticket');
+        if (ticket !== undefined && ticket !== null) {
+          setTicketData(JSON.parse(ticket));
+        }
+      } catch (error) {
+        console.error('Something went wrong while getting Data', error);
+      }
+    };
+
+    getLocalStorageData();
+  }, []);
+
+  if (ticketData === undefined || ticketData === null) {
+    return (
+      <View style={styles.container}>
+        <View style={styles.appHeaderContainer}>
+          <AppHeader
+            name="close"
+            header={'My Tickets'}
+            action={() => navigation.goBack()}
+          />
+        </View>
+      </View>
+    );
+  }
   return (
-    <View>
-      <Text>TicketScreen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.appHeaderContainer}>
+        <AppHeader
+          name="close"
+          header={'My Tickets'}
+          action={() => navigation.goBack()}
+        />
+      </View>
+
+      <View style={styles.ticketContainer}>
+        <ImageBackground
+          source={{uri: ticketData?.ticketImage}}
+          style={styles.ticketBGImage}>
+          <LinearGradient
+            colors={[COLORS.OrangeRGBA0, COLORS.Orange]}
+            style={styles.linearGradient}>
+            <View style={[styles.blackCircle, styles.bottomLeftCircle]} />
+            <View style={[styles.blackCircle, styles.bottomRightCircle]} />
+          </LinearGradient>
+        </ImageBackground>
+        <View style={styles.liner} />
+        <View style={styles.ticketFooter}>
+          <View style={[styles.blackCircle, styles.topLeftCircle]} />
+          <View style={[styles.blackCircle, styles.topRightCircle]} />
+          <View style={styles.ticketDateContainer}>
+            <View>
+              <Text style={styles.dateTitleText}>{ticketData?.date.date}</Text>
+              <Text style={styles.subtitle}>{ticketData?.date.day}</Text>
+            </View>
+            <View>
+              <CustomIcon name="clock" style={styles.clockIcon} />
+              <Text style={styles.subtitle}>{ticketData?.time}</Text>
+            </View>
+          </View>
+
+          <View style={styles.ticketSeatContainer}>
+            <View style={styles.subtitleContainer}>
+              <Text style={styles.subheading}>Hall</Text>
+              <Text style={styles.subtitle}>02</Text>
+            </View>
+            <View style={styles.subtitleContainer}>
+              <Text style={styles.subheading}>Row</Text>
+              <Text style={styles.subtitle}>04</Text>
+            </View>
+            <View style={styles.subtitleContainer}>
+              <Text style={styles.subheading}>Seats</Text>
+              <Text style={styles.subtitle}>
+                {ticketData?.seatArray
+                  .slice(0, 3)
+                  .map((item: any, index: number, arr: any) => {
+                    return item + (index === arr.length - 1 ? '' : ', ');
+                  })}
+              </Text>
+            </View>
+          </View>
+
+          <Image
+            source={require('../assets/image/barcode.png')}
+            style={styles.barcodeImage}
+          />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
 export default TicketScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: COLORS.Black,
+  },
+  appHeaderContainer: {
+    marginHorizontal: SPACING.space_36,
+    marginTop: SPACING.space_20 * 2,
+  },
+  ticketContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    marginVertical: SPACING.space_36,
+    overflow: 'hidden',
+  },
+  ticketBGImage: {
+    alignSelf: 'center',
+    width: 300,
+    aspectRatio: 200 / 300,
+    borderTopLeftRadius: BORDERRADIUS.radius_25,
+    borderTopRightRadius: BORDERRADIUS.radius_25,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
+  },
+  linearGradient: {
+    height: '70%',
+  },
+  liner: {
+    borderTopColor: COLORS.Black,
+    borderTopWidth: 3,
+    width: 300,
+    alignSelf: 'center',
+    backgroundColor: COLORS.Orange,
+    // borderStyle: 'dashed',
+  },
+  ticketFooter: {
+    backgroundColor: COLORS.Orange,
+    width: 300,
+    alignItems: 'center',
+    paddingBottom: SPACING.space_36,
+    alignSelf: 'center',
+    borderBottomLeftRadius: BORDERRADIUS.radius_25,
+    borderBottomRightRadius: BORDERRADIUS.radius_25,
+  },
+  ticketDateContainer: {
+    flexDirection: 'row',
+    gap: SPACING.space_36,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: SPACING.space_10,
+  },
+  dateTitleText: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.White,
+  },
+  subtitleText: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.White,
+  },
+  clockIcon: {
+    fontSize: FONTSIZE.size_24,
+    color: COLORS.White,
+    paddingBottom: SPACING.space_10,
+  },
+  ticketSeatContainer: {
+    flexDirection: 'row',
+    gap: SPACING.space_36,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: SPACING.space_10,
+  },
+  subtitleContainer: {
+    alignItems: 'center',
+  },
+  subtitle: {
+    fontFamily: FONTFAMILY.poppins_regular,
+    fontSize: FONTSIZE.size_14,
+    color: COLORS.White,
+  },
+  subheading: {
+    fontFamily: FONTFAMILY.poppins_medium,
+    fontSize: FONTSIZE.size_18,
+    color: COLORS.White,
+  },
+  barcodeImage: {
+    height: 50,
+    aspectRatio: 158 / 52,
+  },
+  blackCircle: {
+    height: 80,
+    width: 80,
+    borderRadius: 80,
+    backgroundColor: COLORS.Black,
+  },
+  bottomLeftCircle: {
+    position: 'absolute',
+    bottom: -40,
+    left: -40,
+  },
+  bottomRightCircle: {
+    position: 'absolute',
+    bottom: -40,
+    right: -40,
+  },
+  topLeftCircle: {
+    position: 'absolute',
+    top: -40,
+    left: -40,
+  },
+  topRightCircle: {
+    position: 'absolute',
+    top: -40,
+    right: -40,
+  },
+});
